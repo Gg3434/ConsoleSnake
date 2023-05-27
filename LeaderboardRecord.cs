5f11c84e957418165297710995cbf5dce33893e7
giga55 <118477845+gig55tv@users.noreply.github.com>
2023-05-27 10:17:16 +0300

    Update LeaderboardRecord.cs

--- LeaderboardRecord.cs
+++ LeaderboardRecord.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;  // импортирует пространство имен System, которое содержит основные типы и классы .NET Framework.
+using System.Collections.Generic; // импортирует пространство имен  которое содержит обобщенные типы и интерфейсы, такие как List<T>
+using System.Linq; // импортируется пространство имен System.Linq, которое содержит расширения LINQ  для работы с коллекциями и запросами.
+using System.Text; // импортируется пространство имен System.Text, которое содержит классы для работы с различными кодировками и символами.
+using System.Threading.Tasks; // импортирует пространство имен System.Threading.Tasks, которое содержит типы для работы с многопоточностью и синхронизацией.
 
 namespace Snake
 {
     public class LeaderboardRecord
     {
-        public string NamePlayer { get; private set; }
+        public string NamePlayer { get; private set; } 
         public int Score { get; private set; }
         public string Difficulty { get; private set; }
         public int Size { get; private set; }
