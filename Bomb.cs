4787ad7722eae6e13866e8d3f91950453df84d9d
giga55 <118477845+gig55tv@users.noreply.github.com>
2023-05-27 10:14:30 +0300

    Update Bomb.cs

--- Bomb.cs
+++ Bomb.cs
@@ -1,27 +1,27 @@
-﻿using Snake.Model;
-using System;
+using Snake.Model; // импортирует пространство имен Snake.Model, чтобы использовать классы из этого пространства имен в текущем файле.
+using System; // импортирует пространство имен System, которое содержит основные типы и классы .NET Framework.
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // импортируется пространство имен System.Linq, которое содержит расширения LINQ  для работы с коллекциями и запросами.
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; // импортируется пространство имен System.Threading.Tasks, которое содержит классы для работы с асинхронными операциями и параллельным выполнением кода.
 
 namespace Snake
 {
-    public class Bomb : IDrawable
+    public class Bomb : IDrawable 
     {
-        public ConsoleColor Color { get; set; } = ConsoleColor.Red;
-        public Position Position { get; private set; }
-        public char Pixel { get; set ; } = 'x';
+        public ConsoleColor Color { get; set; } = ConsoleColor.Red; // установка красного цвета свойству 
+        public Position Position { get; private set; } // обьявление свойства позиции типа Position
+        public char Pixel { get; set ; } = 'x'; 
 
-        public Bomb(Position position) 
+        public Bomb(Position position) // конструктор класса Bomb принимающий позицию
         {
             Position = position;
         }
-        public void ReLocate(Position position)
+        public void ReLocate(Position position) // метод принимающий позицию 
         {
             Position = position;
         }
-        public List<Position> GetPositions()
+        public List<Position> GetPositions() // метод типа List<Position> для получения новой позиции
         {
             return new List<Position>() { Position };
         } 
