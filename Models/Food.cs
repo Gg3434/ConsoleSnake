7327ad3e994464ac84dfc52695ed480a40531eaf
giga55 <118477845+gig55tv@users.noreply.github.com>
2023-05-27 10:13:34 +0300

    Update Food.cs

--- Models/Food.cs
+++ Models/Food.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Collections.Generic; // импортирует пространство имен  которое содержит обобщенные типы и интерфейсы, такие как List<T>
 
 namespace Snake.Model
 {
     public class Food : IDrawable
     {
-        public Position Position { get; private set; }
-        public ConsoleColor Color { get; set; } = ConsoleColor.Yellow;
+        public Position Position { get; private set; } // свойство позиции типа Position
+        public ConsoleColor Color { get; set; } = ConsoleColor.Yellow; // установка желтого цвета
         public char Pixel { get; set; } = '$';
               
-        public Food(Position position)
+        public Food(Position position) // конструктор принимает и устанавливает позицию  еды
         {
             Position = position;
         }
 
-        public void ReLocate(Position position)
+        public void ReLocate(Position position) // метод перемещения позиции
         {
             Position = position;
         }
 
-        public List<Position> GetPositions()
+        public List<Position> GetPositions() // метод получения позиции
         {
             return new List<Position> { Position };
         }
