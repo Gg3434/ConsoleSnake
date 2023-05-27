671017fcf6484ebb81eadb78ddc9e2219507595d
giga55 <118477845+gig55tv@users.noreply.github.com>
2023-05-27 10:13:50 +0300

    Update IDrawable.cs

--- Models/IDrawable.cs
+++ Models/IDrawable.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Collections.Generic; // импортирует пространство имен  которое содержит обобщенные типы и интерфейсы, такие как List<T>
 
 namespace Snake.Model
 {
     public interface IDrawable
     {
-        public ConsoleColor Color { get; set; }
-        public List<Position> GetPositions();
-        public char Pixel { get; set; }
+        public ConsoleColor Color { get; set; } // свойство цвет который определен в интерфейсе
+        public List<Position> GetPositions(); // метод получения позиций  который определен в интерфейсе
+        public char Pixel { get; set; } // свойство пиксель который определен в интерфейсе
        
     }
 }
